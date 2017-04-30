078d9e6f12194b7aef03b68040c032c7954c59ad
KataokaSusumu <skykataoka@gmail.com>
2017-04-30 11:45:17 +0900

    マイグレーション修正

--- db/migrate/20170425022543_add_title_content_to_topics.rb
+++
@@ -1,7 +0,0 @@
-class AddTitleContentToTopics < ActiveRecord::Migration
-  def change
-    add_reference :topics, :user, foreign_key: true
-    add_column :topics, :title, :string
-    add_column :topics, :content, :text
-  end
-end
