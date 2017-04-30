e8ab5a7b13206425c282524171ee6bf61ae4cc70
KataokaSusumu <skykataoka@gmail.com>
2017-04-30 22:02:55 +0900

    seed edit

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,7 +17,12 @@ require 'csv'
   user.save
 end
 
-
+CSV.foreach('db/facebook.csv') do |row|
+  Topic.create(user_id: User.pluck(:id).sample,
+               title: row[1],
+               content: row[2])
+  sleep(1)
+end
 
 # Commnet
 60.times do
@@ -25,15 +30,8 @@ end
   topic_id = [*1..6].sample
   content = Yoshida::Text.sentence
   Comment.create!(
-    user_id: user_id,
-    topic_id: topic_id,
+    user_id: User.pluck(:id).sample,
+    topic_id: Topic.pluck(:id).sample,
     content: content
   )
-end
-
-CSV.foreach('db/facebook.csv') do |row|
-  Topic.create(user_id: row[0],
-               title: row[1],
-               content: row[2])
-  sleep(1)
 end
